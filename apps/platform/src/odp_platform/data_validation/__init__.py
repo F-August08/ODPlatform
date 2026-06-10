ac5419ed8d4338a1b2e6141720323150fd0941a4
Feng <2438709356@qq.com>
2026-06-10 15:54:02 +0800

    feat: integrate snapshot, report, render into data_validation public API

--- apps/platform/src/odp_platform/data_validation/__init__.py
+++ apps/platform/src/odp_platform/data_validation/__init__.py
@@ -4,7 +4,7 @@
 # @Time      :2026/6/10 09:25:36
 # @Author    :雨霓同学
 # @Project   :ODPlatform
-# @Function  :
+# @Function  :data_validation 公开 API
 from odp_platform.data_validation.registry import (
     CheckContext,
     check,
@@ -12,12 +12,16 @@ from odp_platform.data_validation.registry import (
     CheckSeverity,
     get_check,
     get_all_checks,
-    list_check_names
+    list_check_names,
 )
 
-from odp_platform.data_validation.service import  run_all_checks
+from odp_platform.data_validation.service import run_all_checks
+from odp_platform.data_validation.snapshot import build_snapshot, DatasetSnapshot, SplitStats
+from odp_platform.data_validation.report import ValidationReport
+from odp_platform.data_validation.render import render_to_logger
 
 __all__ = [
+    # -- registry (数据契约 + 注册表) --
     "CheckContext",
     "check",
     "CheckResult",
@@ -25,5 +29,14 @@ __all__ = [
     "get_check",
     "get_all_checks",
     "list_check_names",
-    "run_all_checks"
+    # -- service (调度层) --
+    "run_all_checks",
+    # -- snapshot (数据载体) --
+    "build_snapshot",
+    "DatasetSnapshot",
+    "SplitStats",
+    # -- report (报告产出) --
+    "ValidationReport",
+    # -- render (展示层) --
+    "render_to_logger",
 ]
